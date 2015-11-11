b62c14f49502e3523c67f299d7d76a3db804cc5f
Robert Sapunarich <robertsapunarich@gmail.comgit config --global user.email robertsapunarich@gmail.com>
2015-11-11 22:30:26 +0000

    added devise gem for user authentication created User db model configured mailer to work with devise created routes for user authentication with devise

--- Gemfile
+++ Gemfile
@@ -38,6 +38,9 @@ gem 'bootstrap-sass', '~> 3.3.4'
 # Include factory_girl_rails
 gem "factory_girl_rails", "~> 4.0"
 
+# Include devise for user authentication
+gem 'devise'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.1.2'
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Flixter::Application.configure do
+  config.action_mailer.default_url_options = { :host => 'flixter-robert-sapunarich.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
