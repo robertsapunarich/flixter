806e5765abc220c97edc06488c931c88f3e49b64
Robert Sapunarich <robertsapunarich@gmail.comgit config --global user.email robertsapunarich@gmail.com>
2015-11-17 01:26:33 +0000

    wrote "redirect unenrolled user" test moved "redirect unenrolled user" test from enrollments_controller_test to lessons_controller_test

--- test/controllers/enrollments_controller_test.rb
+++ test/controllers/enrollments_controller_test.rb
@@ -1,13 +1,11 @@
 require 'test_helper'
 
 class EnrollmentsControllerTest < ActionController::TestCase
-  test "enroll user" do
-    c = FactoryGirl.create(:course)
-    u = User.create(email: "rsapunarich456@gmail.com", password: "password")
-    sign_in u
-    get :create
-    assert_response :success  
-  end
-
-  #test redirect unenrolled user
+  #test "enroll user" do
+  #  c = FactoryGirl.create(:course)
+  #  u = User.create(email: "rsapunarich456@gmail.com", password: "password")
+  #  sign_in u
+  #  get :create
+  #  assert_response :success  
+  #end 
 end
--- test/controllers/lessons_controller_test.rb
+++ test/controllers/lessons_controller_test.rb
@@ -6,4 +6,15 @@ class LessonsControllerTest < ActionController::TestCase
     get :show, :id => l.id
     assert_response :success
   end
+
+  test "redirect unenrolled user" do
+    c = FactoryGirl.create(:course)
+    s = Section.create(:course_id => c.id)
+    l = Lesson.create(:title => 'programming', :subtitle => 'how and why', :section_id => s.id)
+    u = FactoryGirl.create(:user)
+    sign_in u
+    get :show, :id => l.id
+    u.enrolled_in?(c)
+    assert_redirected_to course_path 
+  end
 end
