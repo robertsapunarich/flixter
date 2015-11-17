9fb442c3380e37cb67f7c12894440fa8a3173103
Robert Sapunarich <robertsapunarich@gmail.comgit config --global user.email robertsapunarich@gmail.com>
2015-11-17 03:03:10 +0000

    added before_action requiring a user to be enrolled in a course before accessing its lessons and redirects user to course detail page

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, :only => [:show]
+
   def show
   end
 
@@ -7,4 +10,12 @@ class LessonsController < ApplicationController
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
+  def require_authorized_for_current_lesson
+    current_course = current_lesson.section.course
+    if ! current_lesson.section.course.user.enrolled_in?(current_course)
+      redirect_to course_path(current_course), :alert => "You need to enroll to continue"
+    end
+  end
+
 end
--- test/controllers/lessons_controller_test.rb
+++ test/controllers/lessons_controller_test.rb
@@ -1,20 +1,23 @@
 require 'test_helper'
 
 class LessonsControllerTest < ActionController::TestCase
-  test "lesson show page" do
-    l = FactoryGirl.create(:lesson)
-    get :show, :id => l.id
-    assert_response :success
-  end
+  #test "lesson show page" do
+  #  l = FactoryGirl.create(:lesson)
+  #  get :show, :id => l.id
+  #  assert_response :success
+  #end
 
   test "redirect unenrolled user" do
     c = FactoryGirl.create(:course)
-    s = Section.create(:course_id => c.id)
-    l = Lesson.create(:title => 'programming', :subtitle => 'how and why', :section_id => s.id)
+    s = Section.create(course: c)
+    l = Lesson.create(:title => 'programming', :subtitle => 'how and why', section: s)
     u = FactoryGirl.create(:user)
     sign_in u
     get :show, :id => l.id
-    u.enrolled_in?(c)
-    assert_redirected_to course_path 
+    if l.s.c.u.enrolled_in?(c)
+      assert_response :success
+    else
+      assert_redirected_to course_path(c)
+    end 
   end
 end
