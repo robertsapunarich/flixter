4a5f4448d1f6c572f30ad015817ad51c262c4b89
Robert Sapunarich <robertsapunarich@gmail.comgit config --global user.email robertsapunarich@gmail.com>
2015-11-21 22:37:15 +0000

    add row_order column to section database tabel include RankedModel in section model add index on section and row_order add resources for instructor/sections_controller update action in routes.rb add update action to instructor/sections_controller secure update action to ensure that only the user who created the course can access the update action

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Flixter::Application.routes.draw do
   resources :lessons, :only => [:show]
   namespace :instructor do
     resources :lessons, :only => [:update]
+    resources :sections, :only => [:update]
     resources :sections, :only => [] do 
       resources :lessons, :only => [:new, :create]
     end
