faa7436328a3e3f929287aaf08f9a218f78351d4
Robert Sapunarich <robertsapunarich@gmail.comgit config --global user.email robertsapunarich@gmail.com>
2015-11-23 23:00:33 +0000

    create singleton for user dashboard in routes.rb generate dashboards controller and show action create and style dashboard show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Flixter::Application.routes.draw do
   devise_for :users
+  resource :dashboard, :only => [:show]
   root 'static_pages#index'
   resources :courses, :only => [:index, :show] do
     resources :enrollments, :only => :create
