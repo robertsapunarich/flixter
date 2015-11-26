ee049ff89cd454b904ad3ad6fcebd9d821e2a82f
Robert Sapunarich <robertsapunarich@gmail.comgit config --global user.email robertsapunarich@gmail.com>
2015-11-26 19:17:09 +0000

    add careers, team, and privacy actions to static pages controller hook up routes for static pages add links to static pages in footer create views for careers, privacy, and team static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Flixter::Application.routes.draw do
   devise_for :users
   resource :dashboard, :only => [:show]
   root 'static_pages#index'
+  get 'privacy', :to => 'static_pages#privacy'
+  get 'careers', :to => 'static_pages#careers'
+  get 'team', :to => 'static_pages#team'
   resources :courses, :only => [:index, :show] do
     resources :enrollments, :only => :create
   end
